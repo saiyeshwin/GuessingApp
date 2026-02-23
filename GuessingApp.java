566b463745bf24df160be08a98ad9023a5e09d24
saiyeshwin <saiyeshwin@gmail.com>
2026-02-23 15:06:55 +0530

    Coded UC2 to validate the guess of user

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,14 +1,28 @@
-// Guessing App Use Case 1-Game Initialization
-// This class is the entry point
-// It initialzes game configuration and displays the rules of game
-// No user input or game logic is implemented as of now
+// Guessing App Use Case 2-User Guess Submission
+// It  takes user input for the guess and validates it
 // @author Developer
-// @version 1.0
-import java.util.Random;
+// @version 2.0
+
+import java.util.Scanner;
 public class GuessingApp{
 	public static void main(String[] args){
-			System.out.println("Welcome to guessing app");
-			GameConfig gameConfig=new GameConfig();
-			gameConfig.showRules();
+		System.out.println("Welcome to guessing app");
+		GameConfig game = new GameConfig();
+		game.showRules();
+		
+		Scanner sc = new Scanner(System.in);
+		int attempts = 0;
+		
+		while(attempts < game.getMaxAttempts()){
+			System.out.print("Enter your guess");
+			int guess = sc.nextInt();
+			attempts++;
+			
+			String result = GuessValidator.validateGuess(guess, game.getTargetNumber());
+			System.out.println(result);
+			
+			if("correct".equals(result)){
+				break;
+			}
 		}
 }
